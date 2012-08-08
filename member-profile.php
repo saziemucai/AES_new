df4abf88a7f2d5518431a7a0026b101d3d406390
janefaith <janefaith.abanid@gmail.com>
2012-08-08 12:47:39 +0800

    Update member-profile.php

--- member-profile.php
+++ member-profile.php
@@ -1,5 +1,6 @@
 <?php
 	require_once('auth.php');
+	include('config.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -11,6 +12,20 @@
 <body>
 <h1>My Profile </h1>
 <a href="member-index.php">Home</a> | <a href="logout.php">Logout</a>
-<p></p>
+<p>
+<div class="content">
+<h3><font color = "gray"><?php echo $_SESSION['SESS_NAME'];?></h3></br>  
+
+<?php
+$sql = mysql_query("SELECT * FROM members");
+while($row = mysql_fetch_array($members)){ 
+									echo '.$row['idno'].';
+									echo '$row['courseyr']';
+									echo '$row['subject']';
+									echo '$row['section']'; ?>
+</font>
+</div>
+</p>
+
 </body>
 </html>
