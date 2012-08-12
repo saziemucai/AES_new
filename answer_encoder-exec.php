79953bdf7e43ce5d66ba1985e641f40c65a13408
saziemucai <saziemucai@gmail.com>
2012-08-13 01:41:21 +0800

    answer_encoder
    updated.

--- answer_encoder-exec.php
+++ answer_encoder-exec.php
@@ -6,17 +6,17 @@
 	require_once('config1.php');
 	
 	
-	@$number1 = $_POST['number1'];
-	@$number2 = $_POST['number2'];
-	@$number3 = $_POST['number3'];
-	@$number4 = $_POST['number4'];
-	@$number5 = $_POST['number5'];
-	@$number6 = $_POST['number6'];
-	@$number7 = $_POST['number7'];
-	@$number8 = $_POST['number8'];
-	@$number9 = $_POST['number9'];
-	@$number10 = $_POST['number10'];
-	@$answer = $__POST['answer'];
+	$number1 = $_POST['number1'];
+	$number2 = $_POST['number2'];
+	$number3 = $_POST['number3'];
+	$number4 = $_POST['number4'];
+	$number5 = $_POST['number5'];
+	$number6 = $_POST['number6'];
+	$number7 = $_POST['number7'];
+	$number8 = $_POST['number8'];
+	$number9 = $_POST['number9'];
+	$number10 = $_POST['number10'];
+	$answer = $__POST['answer'];
 	
 	//Create INSERT query
 	$qry = "INSERT INTO answerkey(number, answer) VALUES('$number','$answer')";
