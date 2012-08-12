9b469a2adf3e644f323afb179f7bce999a4bf490
saziemucai <saziemucai@gmail.com>
2012-08-13 01:29:26 +0800

    answer_encoded-exec
    debugged.

--- answer_encoder-exec.php
+++ answer_encoder-exec.php
@@ -4,18 +4,19 @@
 	
 	//Include database connection details
 	require_once('config1.php');
-
-	$number1 = $_GET['number1'];
-	$number2 = $_GET['number2'];
-	$number3 = $_GET['number3'];
-	$number4 = $_GET['number4'];
-	$number5 = $_GET['number5'];
-	$number6 = $_GET['number6'];
-	$number7 = $_GET['number7'];
-	$number8 = $_GET['number8'];
-	$number9 = $_GET['number9'];
-	$number10 = $_GET['number10'];
-	$answer = $_GET['answer'];
+	
+	
+	@$number1 = $_POST['number1'];
+	@$number2 = $_POST['number2'];
+	@$number3 = $_POST['number3'];
+	@$number4 = $_POST['number4'];
+	@$number5 = $_POST['number5'];
+	@$number6 = $_POST['number6'];
+	@$number7 = $_POST['number7'];
+	@$number8 = $_POST['number8'];
+	@$number9 = $_POST['number9'];
+	@$number10 = $_POST['number10'];
+	@$answer = $__POST['answer'];
 	
 	//Create INSERT query
 	$qry = "INSERT INTO answerkey(number, answer) VALUES('$number','$answer')";
