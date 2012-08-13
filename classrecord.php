6d2eed05cd12af388849acc666588ab2bfcfd92b
saziemucai <saziemucai@gmail.com>
2012-08-14 00:10:31 +0800

    added new files
    classrecord.php
    profaccount.php

---
+++ classrecord.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Member Index</title>
+<link href="loginmodule.css" rel="stylesheet" type="text/css" />
+</head>
+
+<style type="text/css">
+
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover font{
+	color: #fff;
+}
+a:hover img {
+	color: #fff;
+	}
+a:active {
+	text-decoration: none;
+}
+
+</style>
+
+<body background="gray.jpg">
+<p align="right"><a href="profaccount.php">Home</a> | <a href="logout.php">Logout</p></a>
+<hr>
+</hr>
+<font size="3">
+<i><a href="classrecord.php">CLASS RECORD</a> | <a href="answerkey.php">ANSWER KEY</a> | <a href="examchecker.php">CHECK EXAMS</a></i>
+</font>
+<p>Section:</p >
+<style type="text/css">
+#section
+{
+font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+width:100%;
+border-collapse:collapse;
+}
+#section td, #section th
+{
+font-size:1em;
+border:1px solid #98bf21;
+padding:3px 7px 2px 7px;
+}
+#section th
+{
+font-size:1.1em;
+text-align:left;
+padding-top:4px;
+padding-bottom:3px;
+background-color:#6495ed;
+color:#ffffff;
+}
+#section tr.alt td
+{
+color:#000000;
+background-color:#9999cc;
+}
+</style>
+</head>
+
+<body>
+<table id="section">
+<tr>
+<th>NAME</th>
+<th>PRELIM</th>
+<th>MIDTERM</th>
+<th>FINALS</th>
+</tr>
+
+</font>
+</body>
+</html>
---
+++ profaccount.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Member Index</title>
+<link href="loginmodule.css" rel="stylesheet" type="text/css" />
+</head>
+
+<style type="text/css">
+
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover font{
+	color: #fff;
+}
+a:hover img {
+	color: #fff;
+	}
+a:active {
+	text-decoration: none;
+}
+
+</style>
+
+<body background="gray.jpg">
+<p align="right"><a href="logout.php">Logout</p></a>
+<hr>
+</hr>
+<font size="3">
+<i><br><a href="classrecord.php">CLASS RECORD</a></br>
+<br><a href="answerkey.php">ANSWER KEY</a></br>
+<br><a href="examchecker.php">CHECK EXAMS</a>
+</font>
+</body>
+</html>
