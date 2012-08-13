a04b78934bd99e7ccd745d31b09505aa1cc22d97
rosemarytayone <senpai.mizuki@gmail.com>
2012-08-14 02:46:08 +0800

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php 
  
  //This is the directory where images will be saved 
- $target = "C:xampp/htdocs/wah/images/"; 
+ $target = "C:xampp/htdocs/zaes_rose/AES_new/profilepics/"; 
  $target = $target . basename( $_FILES['photo']['name']); 
  
  //This gets all the other information from the form 
@@ -10,10 +10,10 @@
  
  // Connects to your Database 
  mysql_connect("localhost", "root", "") or die(mysql_error()) ; 
- mysql_select_db("employees") or die(mysql_error()) ; 
+ mysql_select_db("aes") or die(mysql_error()) ; 
  
  //Writes the information to the database 
- mysql_query("INSERT INTO `employees` VALUES ('$name', '$photo')") ; 
+ mysql_query("INSERT INTO `students` VALUES ('$name', '$photo')") ; 
  
  //Writes the photo to the server 
  if(move_uploaded_file($_FILES['photo']['tmp_name'], $target)) 
